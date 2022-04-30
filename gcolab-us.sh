98c4d1bf3ed55e5030316ba242bfb8323b97e3b9
ADYZM <46257040+ADYZM@users.noreply.github.com>
2022-04-30 23:35:53 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o 168.235.86.33:3393 -u SK_QzApkbVGsAxyQykaWSnEF.adyzza -p x -k --coin monero -a cn/r
+./xmrig -o us-west.minexmr.com:443 -u 89AZB4nEHnyRitiFn5cj7BiBR9b8jXhrrChzrfdmi1kpdt8kr6JiPx82jyseExV4gQTvrEu8DhXzCdpPrD7q2hA5AcmQDrK -k --tls --rig-id GC-US
